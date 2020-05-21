c85ddf2ccc3362e1ca9e59f71f26189676318b6e
Vladimir Savrov <i@sava.team>
2020-05-22 01:56:00 +0300

    added copyright

--- Magento/B24Chat/Integration/Block/Script.php
+++ Magento/B24Chat/Integration/Block/Script.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Copyright © B24 Chat. All rights reserved.
+ *
+ * PHP version 7
+ *
+ * @category B24Chat
+ * @package  B24Chat_Integration
+ * @author   Vladimir Savrov <i@sava.team>
+ */
 
 namespace B24Chat\Integration\Block;
 
