ff96b42df17087e6c3d9490c06e74bb88d4cdde2
Vladimir Savrov <i@sava.team>
2020-09-17 21:45:03 +0300

    Magento 1.x: fixed bug in rest api

--- Magento/1.x/app/code/community/B24Chat/Integration/Model/Api2/ReportViewedProducts/Rest.php
+++ Magento/1.x/app/code/community/B24Chat/Integration/Model/Api2/ReportViewedProducts/Rest.php
@@ -12,27 +12,38 @@ class B24Chat_Integration_Model_Api2_ReportViewedProducts_Rest extends B24Chat_I
         $cache = Mage::getSingleton('core/cache');
         $key = 'B24Chat_Integration';
 
-        if ($results = $cache->load($key)) {
+        if (!$results = $cache->load($key)) {
             $results = unserialize($results);
         } else {
             $resource = Mage::getSingleton('core/resource');
             $readConnection = $resource->getConnection('core_read');
 
+            $reportsViewedProductIndexTable = $resource->getTableName('reports/viewed_product_index');
+            $catalogProductEntityIntTable = $resource->getTableName('catalog_product_entity_int');
+            $catalogProductEntityTable = $resource->getTableName('catalog_product_entity');
+            $eavAttributeTable = $resource->getTableName('eav/attribute');
+            $catalogInventoryStockItemTable = $resource->getTableName('cataloginventory/stock_item');
+            $catalogInventoryStockStatusTable = $resource->getTableName('cataloginventory/stock_status');
+
             $query = 'SELECT customer_id AS userId, Viewed.product_id AS productId, rating, timestamp FROM '
                 . '(SELECT customer_id, product_id, UNIX_TIMESTAMP(added_at) AS timestamp FROM '
-                . $resource->getTableName('reports/viewed_product_index') . ' WHERE customer_id IS NOT NULL ) AS Viewed '
+                . $reportsViewedProductIndexTable . ' WHERE customer_id IS NOT NULL ) AS Viewed '
                 . 'JOIN (SELECT product_id, count(product_id) AS rating FROM '
-                . $resource->getTableName('reports/viewed_product_index') . ' '
+                . $reportsViewedProductIndexTable . ' '
 
                 // Active
-                . 'JOIN (SELECT * FROM catalog_product_entity_int WHERE attribute_id = (SELECT attribute_id FROM '
-                . 'eav_attribute WHERE attribute_code LIKE "status") AND value = 1) AS ACTIVE ON product_id = ACTIVE.entity_id '
+                . 'JOIN (SELECT * FROM ' . $catalogProductEntityIntTable . ' '
+                . 'WHERE attribute_id = (SELECT attribute_id FROM ' . $eavAttributeTable . ' '
+                . 'WHERE attribute_code LIKE "status") AND value = 1) AS ACTIVE ON product_id = ACTIVE.entity_id '
 
                 // Stock
-                . 'JOIN (SELECT catalog_product_entity.entity_id, cataloginventory_stock_item.qty, stock_status FROM catalog_product_entity '
-                . 'JOIN cataloginventory_stock_item ON catalog_product_entity.entity_id = cataloginventory_stock_item.product_id '
-                . 'JOIN cataloginventory_stock_status ON catalog_product_entity.entity_id = cataloginventory_stock_status.product_id '
-                . 'WHERE cataloginventory_stock_item.qty > 0 AND stock_status = 1) AS STOCK ON product_id = STOCK.entity_id '
+                . 'JOIN (SELECT ' . $catalogProductEntityTable . '.entity_id, ' . $catalogInventoryStockItemTable
+                . '.qty, stock_status FROM ' . $catalogProductEntityTable . ' '
+                . 'JOIN ' . $catalogInventoryStockItemTable . ' ON ' . $catalogProductEntityTable . '.entity_id = '
+                . $catalogInventoryStockItemTable . '.product_id ' . 'JOIN ' . $catalogInventoryStockStatusTable . ' '
+                . 'ON ' . $catalogProductEntityTable . '.entity_id = ' . $catalogInventoryStockStatusTable
+                . '.product_id WHERE ' . $catalogInventoryStockItemTable . '.qty > 0 AND stock_status = 1) AS STOCK '
+                . 'ON product_id = STOCK.entity_id '
 
                 . 'GROUP BY product_id) AS Rating ON Viewed.product_id = Rating.product_id';
 
